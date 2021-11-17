b2e079dd6d0c04b716be17a9778c51fbfad083fe
danny-flahive <93837720+danny-flahive@users.noreply.github.com>
2021-11-17 16:30:08 +0000

    Added basic logic for part 1

--- SupportBank/Program.cs
+++ SupportBank/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 
 namespace SupportBank
 {
@@ -6,7 +9,25 @@ namespace SupportBank
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string path = @"C:\Users\Danny.Flahive\Corndel\SupportBank\SupportBank\Transactions2014.csv";
+            string[] transactions = File.ReadAllLines(path);
+            //string[] headers = transactions[0].Split(",");
+            Dictionary<string, decimal> balances = new Dictionary<string, decimal>();
+            foreach (string transaction in transactions.Skip(1))
+            {
+                string[] transactionBreakdown = transaction.Split(",");
+                DateTime date = DateTime.Parse(transactionBreakdown[0]);
+                string fromPerson = transactionBreakdown[1];
+                string toPerson = transactionBreakdown[2];
+                string detail = transactionBreakdown[3];
+                decimal amount = decimal.Parse(transactionBreakdown[4]);
+                balances[fromPerson] = balances.ContainsKey(fromPerson) ? balances[fromPerson] - amount : 0 - amount;
+                balances[toPerson] = balances.ContainsKey(toPerson) ? balances[toPerson] + amount : 0 + amount;
+            }
+            foreach (var pair in balances)
+            { 
+                Console.WriteLine($"{pair.Key} has a balance of {pair.Value:C}");
+            }
         }
     }
 }
